02960785a5c12e0eb4e8b5b79eb4dc68b6fdc173
andtsuba <garde4@gmail.com>
2022-04-07 04:54:56 +0300

    First commit

---
+++ bubble_sort.rb
@@ -0,0 +1,21 @@
+def bubble_sort(array)
+  array_length = array.size
+  return array if array_length <= 1
+
+  loop do
+    swapped = false
+
+    (array_length - 1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+    end
+
+    break unless swapped
+  end
+
+  array
+end
+
+p bubble_sort([2, 54, 2, 534])
